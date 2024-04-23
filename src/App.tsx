b4cdd33a0c39592b5bf5fbde3010fc5facfd2e51
ashutosh887 <ashutoshj887@gmail.com>
2024-04-23 19:51:58 +0530

    fix: theme

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
       <iframe
         width="400"
         height="600"
-        src="https://global-stg.transak.com?apiKey=d79671a4-b021-4a4f-a444-6862a680a94b&environment=STAGING&themeColor=FFA500"
+        src="https://global-stg.transak.com?apiKey=d79671a4-b021-4a4f-a444-6862a680a94b&environment=STAGING"
         allow="camera;microphone;payment"
       >
       </iframe>
