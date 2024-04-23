b3f6b333d70a9063fe807870d5f16a5d4382b9d3
ashutosh887 <ashutoshj887@gmail.com>
2024-04-23 19:35:56 +0530

    init: transak-double-iframe-supporter

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+export default function App() {
+
+  return (
+    <main>
+      <iframe
+        width="400"
+        height="600"
+        src="https://global-stg.transak.com?apiKey=d79671a4-b021-4a4f-a444-6862a680a94b&environment=STAGING&themeColor=FFA500"
+        allow="camera;microphone;payment"
+      >
+      </iframe>
+    </main>
+  )
+}
