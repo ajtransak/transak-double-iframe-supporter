9f9da7e2b98be6e6a9946d370151b17b8c788cb7
ashutosh887 <ashutoshj887@gmail.com>
2024-04-23 23:10:01 +0530

    feat: add router config

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,47 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import "./App.css";
 
-export default function App() {
+export const Staging = () => {
   return (
-    <main>
-      <iframe
-        width="400"
-        height="600"
-        src="https://global-stg.transak.com?apiKey=d79671a4-b021-4a4f-a444-6862a680a94b&environment=STAGING"
-        allow="camera;microphone;payment"
-      ></iframe>
-    </main>
+    <iframe
+      width="400"
+      height="600"
+      src="https://global-stg.transak.com"
+      allow="camera;microphone;payment"
+    ></iframe>
   );
+};
+
+export const Production = () => {
+  return (
+    <iframe
+      width="400"
+      height="600"
+      src="https://global.transak.com"
+      allow="camera;microphone;payment"
+    ></iframe>
+  );
+};
+
+export const Home = () => {
+  return <div>Hello world!</div>;
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/staging",
+    element: <Staging />,
+  },
+  {
+    path: "/production",
+    element: <Production />,
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
