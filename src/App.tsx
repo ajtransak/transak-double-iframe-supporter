03ab9159c8d14a1d29b7376ba7848fb7f4d39e5d
ashutosh887 <ashutoshj887@gmail.com>
2024-04-24 14:02:32 +0530

    add: api key handling

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,35 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  useSearchParams,
+} from "react-router-dom";
 import "./App.css";
 
 export const Staging = () => {
+  const [searchParams] = useSearchParams();
+  const apiKey = searchParams.get("apiKey") || " ";
+
   return (
     <iframe
       width="400"
       height="600"
-      src="https://global-stg.transak.com"
+      src={`https://global-stg.transak.com?apiKey=${encodeURIComponent(
+        apiKey
+      )}`}
       allow="camera;microphone;payment"
     ></iframe>
   );
 };
 
 export const Production = () => {
+  const [searchParams] = useSearchParams();
+  const apiKey = searchParams.get("apiKey") || " ";
+
   return (
     <iframe
       width="400"
       height="600"
-      src="https://global.transak.com"
+      src={`https://global.transak.com?apiKey=${encodeURIComponent(apiKey)}`}
       allow="camera;microphone;payment"
     ></iframe>
   );
